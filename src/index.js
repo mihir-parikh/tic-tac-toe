d29169786b58765944153c31a0fed1aa076e7fdc
Mihir Parikh <mihir1408@gmail.com>
2022-12-07 20:03:27 +1100

    Flip the flag & take turns

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 function Square(props) {
     return (
-        <button className="square" onClick={() => props.onClick}>
+        <button className="square" onClick={() => props.onClick()}>
             {props.value}
         </button>
     );
@@ -14,14 +14,21 @@ class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            squares: Array(9).fill(null)
+            squares: Array(9).fill(null),
+            // The initial default value.
+            xIsNext: true,
         };
     }
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({squares: squares});
+        // Fill the square
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            squares: squares, 
+            // Flip
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
     renderSquare(i) {
